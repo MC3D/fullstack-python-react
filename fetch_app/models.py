c59e9d7dd28aeb858fe715703fd6dc92e289a9e9
MC3D <m.chapman.home@gmail.com>
2018-07-18 20:29:08 -0400

    adds profile class

--- fetch_app/models.py
+++ fetch_app/models.py
@@ -5,3 +5,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
